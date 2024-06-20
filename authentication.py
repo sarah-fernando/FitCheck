87b73ec10c9fc9660df91482ddc6185522b29c07
ArunSC <arun02.sc@gmail.com>
2024-06-19 21:54:09 -0400

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,7 +1,5 @@
-import os
 
-
-def email_exists (email, filename='accounts.txt'):
+def email_exists (email, filename='database/accounts.txt'):
     accounts_file = open(filename, 'r')
     for line in accounts_file:
         parts = line.strip().split(",")
@@ -21,8 +19,8 @@ def create_new_account ():
     if email_exists(email):
         print (f"Error: The email {email} is already registered.")
     else:
-        accounts_file = open('accounts.txt', 'a')
-        info = f"{email},{password}"
+        accounts_file = open('database/accounts.txt', 'a')
+        info = f"{email},{password}\n"
         accounts_file.writelines(info)
         print ("Account Created Successfully!")
         accounts_file.close()
@@ -30,43 +28,53 @@ def create_new_account ():
 
 
 def login_to_account ():
-    email = input ("Enter your email: ")
-    if email_exists(email):
-        password = input ("Enter your password: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    if email_exists(email) == False:
+        print("Email or password incorrect. Please try again.")
+        return False
 
-    accounts_file = open('accounts.txt', 'r')
+    accounts_file = open('database/accounts.txt', 'r')
     for line in accounts_file:
         parts = line.strip().split(",")
         email_check = parts[0]
         password_check = parts[1]
 
     if email_check == email and password_check == password:
-        print ("Logged in successfully!")
+        print("Logged in successfully!")
         accounts_file.close()
         return True
     
     accounts_file.close()
 
-    print ("Email or password incorrect. Please try again.")
+    print("Email or password incorrect. Please try again.")
     return False
         
 
 
 def main():
     while True:
-        print ("1. Create an account")
-        print ("2. Login to account") 
-        choice = input ("Enter your choice: ")
+        print("----------FitCheck----------")
+        print()
+        print("1. Create an account")
+        print("2. Login to account") 
+        print("3: Quit")
+        print()
+        print("----------FitCheck----------")
+        print()
+        choice = input("Enter your choice: ")
 
         if choice == '1':
             create_new_account()
         elif choice == '2':
             login_to_account()
             break
+        elif choice == '3': 
+            break
         else:
             print("Invalid choice, please try again.")
 
 
-  
+
 if __name__ == '__main__':
     main()
